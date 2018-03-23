193778e6de3d9dd379ba74ad573486ac331ddcb0
Grant Gloster <ggloster@hotmail.com>
2018-03-23 20:53:15 +0200

    MongoDB

---
+++ nodetest/routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// Get Hello World page.
+router.get('/helloworld', function (req, res) {
+  res.render('helloworld', { title:  'Hello, World!' });
+});
+
+router.get('/userlist', function (req, res) {  
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{}, function(e,docs){
+    res.render('userlist', {
+      "userlist": docs
+    });
+  });
+});
+
+router.get('/newuser', function (res, res) {
+  res.render('newuser', {
+    title: 'Add New User'
+  });
+});
+
+router.post('/adduser', function (req, res) {
+  var db = req.db;
+
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  var collection = db.get('usercollection');
+
+  collection.insert({
+    "username": userName,
+    "email": userEmail
+  }, function (err, doc) {
+    if (err) {
+      res.send('There was a problem adding user.');
+    }
+    else {
+      res.redirect("userlist");
+    }
+  });
+});
+
+module.exports = router;
